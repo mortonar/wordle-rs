9c95b3975c81224f315aa399619a5f8c8b71042f
Alex Morton <mortonar01@gmail.com>
2022-05-18 13:31:21 -0400

    fetch random word from REST API & clippy cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use std::io::{self, Write};
-use std::ops::Index;
-use std::process::exit;
 use clap::Parser;
+use curl::easy::Easy;
+use serde_json::Value::{self, Array};
+use std::io::{self, Write};
+use std::process::exit;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     /// print debugging info (spoils the answer!)
     #[clap(short, long)]
-    debug: bool
+    debug: bool,
 }
 
 fn main() {
@@ -18,7 +18,7 @@ fn main() {
     // TODO read one char at a time and disallow non english alphabet characters
     let stdin = io::stdin();
     // TODO replace with random word from dictionary
-    let word = "wordl";
+    let word = fetch_word();
     let cli = Cli::parse();
     if cli.debug {
         println!("DEBUG: word: {}", word);
@@ -42,12 +42,12 @@ fn main() {
             exit(3);
         }
 
-        if in_buffer.trim().eq(word) {
+        if in_buffer.trim().eq(&word) {
             println!("correct!");
             stdout
                 .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
                 .unwrap();
-            writeln!(&mut stdout, "{}", &word);
+            writeln!(&mut stdout, "{}", &word).unwrap();
             exit(0);
         } else {
             println!("incorrect! {} tries remaining", n);
@@ -65,9 +65,39 @@ fn main() {
                 stdout
                     .set_color(ColorSpec::new().set_fg(Some(color)))
                     .unwrap();
-                write!(&mut stdout, "{}", &c);
+                write!(&mut stdout, "{}", &c).unwrap();
             }
-            writeln!(&mut stdout, "");
+            writeln!(&mut stdout).unwrap();
         }
+    }
+}
+
+fn fetch_word() -> String {
+    let mut dst = Vec::new();
+    {
+        let mut easy_curl = Easy::new();
+        easy_curl
+            .url("https://random-word-api.herokuapp.com/word?length=5")
+            .unwrap();
+        let mut transfer = easy_curl.transfer();
+        transfer
+            .write_function(|data| {
+                dst.extend_from_slice(data);
+                Ok(data.len())
+            })
+            .unwrap();
+        transfer.perform().unwrap();
+    }
+    let content = std::str::from_utf8(&dst).unwrap();
+    let parsed: Value = serde_json::from_str(content).unwrap();
+    if let Array(inner_vec) = parsed {
+        let first_el = inner_vec.get(0).unwrap();
+        if let Value::String(word) = first_el {
+            word.clone()
+        } else {
+            panic!("Didn't get JSON ['word'] back from random-word-api")
+        }
+    } else {
+        panic!("Didn't get JSON ['word'] back from random-word-api")
     }
 }
