69c285029cc99adc60fdadd0dfd15c9285eb136e
Alex Morton <mortonar01@gmail.com>
2022-06-17 14:22:06 -0400

    Download an dictionary of 5-letter words to validate guesses

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,13 +1,39 @@
 use curl::easy::Easy;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use serde_json::Value;
 use serde_json::Value::Array;
 
-pub fn fetch_random_word() -> String {
+#[derive(Debug)]
+pub struct Dictionary {
+    words: Vec<String>,
+}
+
+impl Dictionary {
+    // TODO add error handling when building the dictionary from the REST API fails
+    pub fn initialize() -> Self {
+        Dictionary {
+            words: fetch_words(),
+        }
+    }
+
+    pub fn fetch_random_word(&self) -> &str {
+        let mut rng = thread_rng();
+        self.words.choose(&mut rng).unwrap()
+    }
+
+    pub fn is_valid_word(&self, word: &str) -> bool {
+        self.words.binary_search(&word.to_owned()).is_ok()
+    }
+}
+
+fn fetch_words() -> Vec<String> {
+    let mut return_val = Vec::new();
     let mut dst = Vec::new();
     {
         let mut easy_curl = Easy::new();
         easy_curl
-            .url("https://random-word-api.herokuapp.com/word?length=5")
+            .url("https://random-word-api.herokuapp.com/all")
             .unwrap();
         let mut transfer = easy_curl.transfer();
         transfer
@@ -21,12 +47,18 @@ pub fn fetch_random_word() -> String {
     let content = std::str::from_utf8(&dst).unwrap();
     let parsed: Value = serde_json::from_str(content).unwrap();
     if let Array(inner_vec) = parsed {
-        if let Some(Value::String(word)) = inner_vec.first() {
-            word.clone()
-        } else {
-            panic!("Didn't get JSON ['word'] back from random-word-api")
+        for w in inner_vec {
+            if let Value::String(word) = w {
+                if word.len() == 5 {
+                    return_val.push(word);
+                }
+            } else {
+                panic!("Didn't get JSON ['word'] back from random-word-api")
+            }
         }
     } else {
         panic!("Didn't get JSON ['word'] back from random-word-api")
     }
+    return_val.sort();
+    return_val
 }
--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,29 @@ struct Cli {
 }
 
 fn main() {
-    println!("Enter a 5-letter word: ");
     // TODO read one char at a time and disallow non english alphabet characters
     let mut guess_reader = GuessReader::new(Box::new(io::stdin().lock()));
     let mut guess_writer = ConsoleGuessWriter::new();
-    let word = dictionary::fetch_random_word();
-    let mut game_state = game_state::GameState::new(word.clone());
+    println!("Building dictionary...");
+    let dictionary = dictionary::Dictionary::initialize();
+    let word = dictionary.fetch_random_word();
+    let mut game_state = game_state::GameState::new(word.to_owned());
     let cli = Cli::parse();
     if cli.debug {
         println!("DEBUG: word: {}", &word);
     }
+    println!("Enter a 5-letter word: ");
     loop {
         match guess_reader.get_guess() {
             Ok(guess) => {
-                let guess = game_state.evaluate(guess.trim());
-                guess_writer.write(&guess);
-                if guess.is_terminal() {
-                    break;
+                if dictionary.is_valid_word(guess.trim()) {
+                    let guess = game_state.evaluate(guess.trim());
+                    guess_writer.write(&guess);
+                    if guess.is_terminal() {
+                        break;
+                    }
+                } else {
+                    println!("{} is not a valid word", &guess);
                 }
             }
             // TODO Print these in red / add to the guess writer to print these?
